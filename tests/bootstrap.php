0ca3a0adfebe507842e99e9592dc85a2a1fe49a2
ShuSRun <2771717608@qq.com>
2022-07-02 23:56:47 +0800

    Add:  add bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * this file Author is Death-Satan.
+ * Sms Manage
+ * @homepage https://github.com/Death-Satan
+ * @link https://github.com/Death-Satan/sms-sdk
+ */
+require_once dirname(__FILE__, 2) . '/vendor/autoload.php';
