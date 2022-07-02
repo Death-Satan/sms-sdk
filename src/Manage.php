2d0897ff70e359b65712ff8aea9803bcf612c9de
ShuSRun <2771717608@qq.com>
2022-07-03 00:38:39 +0800

    Fix

--- src/Manage.php
+++ src/Manage.php
@@ -9,6 +9,8 @@ declare(strict_types=1);
  */
 namespace DeathSatan\SmsSdk;
 
+use DeathSatan\SmsSdk\Exceptions\SmsException;
+
 class Manage
 {
     /**
@@ -16,8 +18,61 @@ class Manage
      */
     protected array $config = [
         'handle' => '',
-        'option' => [
-            '',
+        'options' => [
         ],
     ];
+
+    public function __construct(array $config = [])
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @throws SmsException
+     */
+    public function getAdapter(): SmsAdapter
+    {
+        $adapter_name = $this->getAdapterName();
+        $adapter = new $adapter_name($this->getAdapterConfig());
+        if ($adapter instanceof SmsAdapter) {
+            return $adapter;
+        }
+        throw new SmsException('This adapter does not inherit from the adapter class [' . $adapter_name . ']');
+    }
+
+    /**
+     * 一些初始化的操作.
+     */
+    protected function initialize(): void
+    {
+    }
+
+    /**
+     * @throws SmsException
+     */
+    protected function getAdapterName(): string
+    {
+        $name = $this->config['handle'];
+        if (class_exists($name)) {
+            return $name;
+        }
+        throw new SmsException('sms adapter :[' . $name . '] exists');
+    }
+
+    /**
+     * @throws SmsException
+     */
+    protected function getAdapterConfig(): array
+    {
+        $adapter_name = $this->getAdapterName();
+        $config_name = $adapter_name;
+        if (property_exists($adapter_name, 'config_name') && $adapter_name::$config_name !== null) {
+            $config_name = $adapter_name::$config_name;
+        }
+        $options = $this->config['options'];
+        if (empty($options[$config_name])) {
+            throw new SmsException('No configuration found for this class [' . $adapter_name . ']');
+        }
+        return $options[$config_name];
+    }
 }
---
+++ src/SmsAdapter.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * this file Author is Death-Satan.
+ * Sms Manage
+ * @homepage https://github.com/Death-Satan
+ * @link https://github.com/Death-Satan/sms-sdk
+ */
+namespace DeathSatan\SmsSdk;
+
+abstract class SmsAdapter
+{
+    /**
+     * 你的adapter配置名.
+     */
+    public static ?string $config_name = null;
+
+    /**
+     * 配置.
+     */
+    protected array $config = [];
+
+    public function __construct(array $config = [])
+    {
+        $this->config = $config;
+
+        $this->initialize();
+    }
+
+    /**
+     * 当一切准备好后会执行此方法.
+     */
+    protected function initialize(): void
+    {
+    }
+}
