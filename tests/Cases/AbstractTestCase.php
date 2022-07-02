ba41e716a624fc82517cd97a3f78a71f12ab604c
ShuSRun <2771717608@qq.com>
2022-07-02 23:58:04 +0800

    Fix

--- tests/Cases/AbstractTestCase.php
+++ tests/Cases/AbstractTestCase.php
@@ -1,10 +1,20 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * this file Author is Death-Satan.
+ * Sms Manage
+ * @homepage https://github.com/Death-Satan
+ * @link https://github.com/Death-Satan/sms-sdk
+ */
 namespace Cases;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @internal
+ * @coversNothing
+ */
 class AbstractTestCase extends TestCase
 {
-
 }
